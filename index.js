c10cd0d5e956de707111d3af5109dbbb5d70305d
Nayib <basstrombone313@gmail.com>
2024-10-30 11:35:08 -0400

    updated

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 
 function contact(event) {
     event.preventDefault();
-    console.log('it worked')
+    // emailjs
+    //   .sendForm(
+    //     'service_3bjs5fs',
+    //     'template_kv62w9c',
+    //     event.target,
+    //     'enHWFdsVJYjP3hNox'
+    //   ).then(() => {
+    //     console.log('this worked1')
+    //   })
+    const loading = document.querySelector('.modal__overalay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+    setTimeout(() => {
+        loading.classList.remove("modal__overlay--visible")
+        console.log('it worked 1')
+    }, 500)
 }
