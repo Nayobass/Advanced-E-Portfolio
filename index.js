afbd44db91a19e1eef7d719f9957905fd0e393f1
Nayib <basstrombone313@gmail.com>
2024-11-01 14:40:34 -0400

    finished e-portfolio

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 === 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
 
 function contact(event) {
     event.preventDefault();
-    // emailjs
-    //   .sendForm(
-    //     'service_3bjs5fs',
-    //     'template_kv62w9c',
-    //     event.target,
-    //     'enHWFdsVJYjP3hNox'
-    //   ).then(() => {
-    //     console.log('this worked1')
-    //   })
-    const loading = document.querySelector('.modal__overalay--loading')
+    const loading = document.querySelector('.modal__overlay--loading')
     const success = document.querySelector('.modal__overlay--success')
     loading.classList += " modal__overlay--visible"
-    setTimeout(() => {
+    emailjs
+      .sendForm(
+        'service_3bjs5fs',
+        'template_kv62w9c',
+        event.target,
+        'enHWFdsVJYjP3hNox'
+      ).then(() => {
         loading.classList.remove("modal__overlay--visible")
-        console.log('it worked 1')
-    }, 500)
+        success.classList += " modal__overlay--visible"
+      }).catch(() => {
+        loading.classList.remove("modal__overlay--visible")
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on basstrombone313@gmail.com"
+        )
+      })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = !isModalOpen
+    document.body.classList += " modal--open"
 }
